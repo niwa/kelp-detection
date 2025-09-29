4a8db415e6eb9553c2ed7d307f7a92e36cec9fe0
AndraWhiteside <andra.whiteside@niwa.co.nz>
2025-09-29 15:26:35 +1300

    Added lines of code which appeared to be missing
    Added missing lines of code which were included in the latest modifications to the script. This includes running the two pass thresholds - indices + kelp buffer and min pixel removal thresholds.

--- scripts/create_data_NZ_wide_two_pass.py
+++ scripts/create_data_NZ_wide_two_pass.py
@@ -39,13 +39,10 @@ def main():
 
     bands = list(utils.SENTINEL_2B_BAND_INFO.keys()); bands.append("SCL") # bands = ["red", "green", "blue", "nir", "SCL", "swir16", "B05", "B8A"]
     raster_defaults = {"resolution": 10, "nodata": 0, "dtype": "uint16"}
-    thresholds = {"min_ndvi": 0.03, "max_ndwi": 0.1, "max_ndwi2": -0.2,} # "max_ndvi": 0.7, "min_ndvri": 0.03
-    anomaly_thresholds = {"min_ndvi": 0.213, "max_ndwi": 0.1, "max_ndwi2": -0.2} 
-
-
+    thresholds = {"min_ndvi": 0.213, "max_ndwi": 0.1, "max_ndwi2": -0.2,} # "max_ndvi": 0.7, "min_ndvri": 0.03, "min_ndvi": 0.03
+    
     filter_cloud_percentage = 30
     max_ocean_cloud_percentage = 5
-    anomaly_detection_factor = 20
     rgb_bands = utils.get_band_names_from_common(["red", "green", "blue"])
 
 
@@ -62,7 +59,7 @@ def main():
         raster_path.mkdir(parents=True, exist_ok=True)
     
         # Geometry of AOI - convex hull to allow search
-        site_bbox = row.geometry.bounds # shapely.box(*row.geometry.bounds) #.to_crs(utils.CRS_WSG).iloc[0].geometry
+        site_bbox = row.geometry.bounds # shapely.box(*row.geometry.bounds) #.to_crs(utils.CRS_WSG).iloc[0].geometr
 
         filters = {"eo:cloud_cover":{"lt":filter_cloud_percentage}} 
 
@@ -123,31 +120,6 @@ def main():
 
                 # Calculate Kelp from thresholds
                 data = utils.threshold_kelp(data, thresholds, roi)
-
-                # Check for any big differences in kelp area using the anomaly thresholds
-                data["kelp_original"] = data["kelp"].copy(deep=True)
-                data = utils.threshold_kelp(data, anomaly_thresholds, roi)
-                anomalous = data["kelp_original"].notnull().sum(dim=["x", "y"]) > anomaly_detection_factor * data["kelp"].notnull().sum(dim=["x", "y"])
-                if anomalous.any():
-                    anomalous.load()
-                    print(f"\t\tKeeping only dates {data.time[anomalous==False].data} of {data.time.data} as anomalies detected in {data.time[anomalous].data}."
-                          f"Area changes from {data['kelp_original'].notnull().sum(dim=['x', 'y']).load().data} to {data['kelp'].notnull().sum(dim=['x', 'y']).load().data}")
-                    data = data.isel(time=(anomalous==False))  # Keep only those date that are not anomalies
-                data["kelp"] = data["kelp_original"]
-                data = data.drop_vars("kelp_original")
-                
-                # Create buffered area around kelp beds
-                kelp_polygons_buffered = []
-                for index in range(len(data["kelp"].time)):
-                    kelp_polygons_i = utils.polygon_from_raster(data["kelp"].isel(time=index)).explode(ignore_index=True, index_parts=False)
-                    kelp_polygons_i = kelp_polygons_i[kelp_polygons_i.area > min_pixels*(utils.RASTER_DEFAULTS["resolution"]**2)]
-                    kelp_polygons_i = kelp_polygons_i.buffer(utils.RASTER_DEFAULTS["resolution"] * buffer)
-                    kelp_polygons_i = geopandas.GeoDataFrame(geometry=[kelp_polygons_i.unary_union], crs=kelp_polygons_i.crs)
-                    kelp_polygons_i = kelp_polygons_i.overlay(roi, how="intersection", keep_geom_type=True)
-                    kelp_polygons_buffered.append(kelp_polygons_i)
-
-                # Caclulate Kelp from second thresholds - reset data first
-                data = utils.threshold_kelp(data, thresholds, kelp_polygons_buffered)
 
                 # Save each separately
                 for index in range(len(data["kelp"].time)):
